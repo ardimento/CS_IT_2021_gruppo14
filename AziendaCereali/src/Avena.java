31c550816c4583310d0f0891a0f27d38154e653d
BenSidiAlessio <bensidialessio@gmail.com>
2021-02-28 18:59:29 +0100

    Aggiornamento della classe Avena (modifica del contenuo) #4
    Ho completato la classe avena aggiungendo tutti i suoi attributi e
    aggiornando il aggiungendo il parametro "proprietaNutritive" e passando
    alla classe astratta i dovuti attributi

--- AziendaCereali/src/Avena.java
+++ AziendaCereali/src/Avena.java
@@ -1,7 +1,12 @@
 
 public class Avena extends AbstractCereali {
 	
-	public Avena() {
-		super();
+	private final static int GIORNI_FRESCHEZZA = 10;
+	private final static double PREZZO = 10.00;
+	protected final static String NOME_CLASSE = "avena";
+	private final static double QUANTITA_GIORNALIERA_MAX = 30.00;
+	
+	public Avena(String proprietaNutritive) {
+		super(proprietaNutritive, GIORNI_FRESCHEZZA, PREZZO, NOME_CLASSE);
 	}
 }
