f445e660c49ebba6ba53503ab09c0c80b4e5e4c9
AlemannoGiuseppe <giuseppe.alemanno1100@gmail.com>
2021-03-01 17:44:03 +0100

    Creazione della documentazione delle classi create finora #8
    Ho creato la documentazione delle classi :
    ProprietaNutritive
    NomeCereali

--- AziendaCereali/src/NomeCereali.java
+++ AziendaCereali/src/NomeCereali.java
@@ -1,6 +1,14 @@
-
+/**
+ * Classe che contiene i nomi dei Cereali gestiti dal programma.
+ * Questa classe verrà utilizzata dalle classi non logicamente correlate al concetto di Cereale
+ * 
+ * Se si dovessero introdurre altri cereali, 
+ * i loro nomi andranno introdotte in questa classe.
+ * @author Alemanno Giuseppe 716262
+ *
+ */
 public class NomeCereali {
-
+	/**Insieme di valori alfanumerici che rappresentano i nomi dei vari Cereali gestiti dal programma*/
 	public static String [] nomiCereali = {Avena.NOME_CLASSE};
 	
 }
--- AziendaCereali/src/ProprietaNutritive.java
+++ AziendaCereali/src/ProprietaNutritive.java
@@ -1,5 +1,14 @@
-
+/**
+ * Classe che contiene le proprietà nutritive dei Cereali gestiti dal programma.
+ * 
+ * Se si dovessero introdurre altri cereali ai quali corrispondono delle proprietà nutritive, 
+ * tali proprietà andranno introdotte in questa classe.
+ * 
+ * @author Alemanno Giuseppe 716262
+ *
+ */
 public class ProprietaNutritive {
 
+	/**Valore alfanumerico che contiene l'insieme delle proprietà nutritive dell'avena */
 	public static String PROPRIETA_AVENA = "70% carboidrati, 17% grassi,13% proteine e 13% fibre";
 }
