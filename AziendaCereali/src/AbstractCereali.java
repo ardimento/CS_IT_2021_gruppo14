ecc0c0e01b253075fe9f75a9ba18da61a6b6bf54
FortunatoGiuseppe <fortunatogiuseppe1997@gmail.com>
2021-03-01 17:34:51 +0100

    Creazione della documentazione delle classi create finora #8
    Ho realizzato la documentazione della classe AbstractCereali e
    dell'interfaccia Cereale

--- AziendaCereali/src/AbstractCereali.java
+++ AziendaCereali/src/AbstractCereali.java
@@ -1,39 +1,67 @@
 
 public class AbstractCereali implements Cereale {
 	
+	/**
+	 * Valore String che indica le proprieta' nutritive del cereale
+	 */
 	private String proprietaNutritive;
+	/**
+	 * Valore intero che indica i giorni di freschezza del cereale
+	 */
 	private int giorniFreschezza;
+	/**
+	 * Valore Double che indica il prezzo del cereale
+	 */
 	private Double prezzo;
+	/**
+	 * Valore String che indica il nome del cereale
+	 */
 	private String nomeCereale;
 	
+	/**
+	 * Costruttore AbstractCereale
+	 * @param proprietaNutritive stringa che elenca le proprieta' nutritive del cereale
+	 * @param giorniFreschezza intero che indica i giorni di freschezza del cereale
+	 * @param prezzo Double che indica il prezzo del cereale
+	 * @param nomeCereale String che indica il nome del cereale
+	 */
 	public AbstractCereali(String proprietaNutritive, int giorniFreschezza, Double prezzo, String nomeCereale) {
 		this.proprietaNutritive = proprietaNutritive;
 		this.giorniFreschezza = giorniFreschezza;
 		this.prezzo = prezzo;
 		this.nomeCereale = nomeCereale;
 	}
-
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Double getPrezzo() {
 		return prezzo;
 	}
-
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public int getTempoFreschezza() {
 		return giorniFreschezza;
 	}
-
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getProprietaNutritive() {
 		return proprietaNutritive;
 	}
-
+	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String getNomeCereale() {
 		return nomeCereale;
 	}
-	
-	
-	
 	
 }
--- AziendaCereali/src/Cereale.java
+++ AziendaCereali/src/Cereale.java
@@ -1,9 +1,28 @@
 
 public interface Cereale {
 	
+	/**
+	 * Metodo che ritorna il prezzo del cereale
+	 * @return prezzo
+	 */
 	public Double getPrezzo();
+	
+	/**
+	 * Metodo che ritorna il tempo di freschezza di un cereale
+	 * @return giorniFreschezza
+	 */
 	public int getTempoFreschezza();
+	
+	/**
+	 * Metodo che ritorna le porprieta' nutritive del cereale
+	 * @return proprietaNutritive
+	 */
 	public String getProprietaNutritive();
+	
+	/**
+	 * Metodo che ritorna il nome del cereale
+	 * @return nomeCereale
+	 */
 	public String getNomeCereale();
 	
 }
