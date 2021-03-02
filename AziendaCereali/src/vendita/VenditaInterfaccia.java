67471c20c38924b105c30b4967c2ffb224ec996f
BenSidiAlessio <bensidialessio@gmail.com>
2021-03-02 16:33:24 +0100

    Creazione dell'Interfaccia "VenditaInterfaccia" #13
    Creazione dell'interfaccia VenditaInterfaccia
    Aggiunta documentazione per l'interfaccia e per i metodi

---
+++ AziendaCereali/src/vendita/VenditaInterfaccia.java
@@ -0,0 +1,20 @@
+package vendita;
+/**
+ * Interfaccia che è implementata dalla classe @see Vendita dove per la maggior parte ritroviamo metodi che restituiscono
+ * i dati di una singola vendita
+ * 
+ * @author BenSidi Alessio 717848
+ *
+ */
+public interface VenditaInterfaccia {
+	
+	/** fornisce in formato stringa i dati della vendita*/
+	public String toString();
+	/** permette di ricavare la stringa che rappresenta l'elenco delle proprietà nutritive del cereale coinvolto nella vendita*/
+	public String getProprietaNutritive();
+	/** restituisce il valore double in kg del peso della vendita del cereale coinvolto*/
+	public Double getQuantitaCereale();
+	/** restrituisce una stringa con la data della vendita nel formato YYYY-MM-GG*/
+	public String getDataVendita();
+
+}
