c303dba094904865881da120fc71ca4c3bfc216a
AlemannoGiuseppe <giuseppe.alemanno1100@gmail.com>
2021-03-05 17:04:59 +0100

    Creazione del Package immagini #17
    Aggiornamento :
    Ho cambiato il path dell'immagine che utilizzo per il login

--- AziendaCereali/src/immagini/PathImmagini.java
+++ AziendaCereali/src/immagini/PathImmagini.java
@@ -14,7 +14,7 @@ package immagini;
 public class PathImmagini {
 
 	/** valore alfanumerico che rappresenta il path dell'immagine utilizzata nella schermata @see login */
-	public final static String IMMAGINE_LOGIN = "/immagini/immagine1.jpg";
+	public final static String IMMAGINE_LOGIN = "/immagini/immagine2.jpg";
 	/** valore alfanumerico che rappresenta il path dell'immagine utilizzata come icona nella finestra del programma */
 	public final static String IMMAGINE_ICONA_PROGRAMMA = "/immagini/iconaFinestra.png";
 	
