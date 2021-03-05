e51695074c1e255ae3df8217270fc77e252cc0e3
AlemannoGiuseppe <giuseppe.alemanno1100@gmail.com>
2021-03-05 17:27:46 +0100

    Creazione Login impiegato #16
    Aggiornamento :
    - aggiunta l'icona del programma
    - aggiunto il label che conterrà l'immagine
    - aggiunto il metodo che carica se possibile l'immagine di login o in
    caso contrario lancia un'eccezione

--- AziendaCereali/src/gui/Login.java
+++ AziendaCereali/src/gui/Login.java
@@ -7,20 +7,23 @@ import javax.swing.*;
 import impiegato.Impiegato;
 
 /**
+ * Classe che gestisce graficamente l'accesso degli impiegati
+ * alle proprie aree di lavoro
  * 
  * @author Fortunato Giuseppe 724309
- *
+ * @author Giuseppe Alemano 716262
  *
  */
 public class Login extends JFrame{
 	
 	private JFrame login;
-	private JPanel panelImage;
-	private JPanel panelForm;
-	private JLabel labelAccess;
-	private JLabel labelCode;
+	
+	private JPanel panelImage,panelForm;
+	
+	private JLabel labelAccess,labelCode,labelWelcome,labelImage;
+	
 	private JTextField textCode;
-	private JLabel labelWelcome;
+
 	
 	
 	/**
@@ -31,8 +34,27 @@ public class Login extends JFrame{
 		super();
 		this.login = new JFrame();	
 		setLogin();
+		setImageLogin();
 	}
 	
+	
+	/**
+	 * Metodo che imposta l'immagine di login 
+	 * E lancia una eccezione se l'immagine non viene creata.
+	 * 
+	 */
+	private void setImageLogin() {
+		try {
+			ImageIcon immagineIcona = new ImageIcon(Login.class.getResource(immagini.PathImmagini.IMMAGINE_LOGIN));
+			Image immagine = immagineIcona.getImage();
+			Image immagineModificata = immagine.getScaledInstance(labelImage.getWidth(), labelImage.getHeight(), Image.SCALE_SMOOTH);
+			immagineIcona = new ImageIcon(immagineModificata);
+			
+			labelImage.setIcon(immagineIcona);
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Non è stato possibile caricare l'immagine di login", "Errore", JOptionPane.ERROR_MESSAGE);
+		}
+	}
 	/**
 	 * 
 	 */
@@ -44,19 +66,25 @@ public class Login extends JFrame{
 		login.setSize(800,600);
 		login.setLocationRelativeTo(null);
 		login.setBackground(SystemColor.window);
+		login.setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource(immagini.PathImmagini.IMMAGINE_ICONA_PROGRAMMA)));
 		login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		login.getContentPane().setLayout(null);
 		
 		panelImage = new JPanel();
-		panelImage.setBounds(0, 0, 398, 600);
+		panelImage.setBounds(0, 0, login.getWidth()/2, login.getHeight());
 		panelImage.setBackground(new Color(244,164,96));
 		panelImage.setVisible(true);
 		login.getContentPane().add(panelImage);
 		panelImage.setLayout(null);
 		
+		labelImage = new JLabel();
+		labelImage.setBounds(0, 0, panelImage.getWidth(), panelImage.getHeight());
+		panelImage.add(labelImage);
+	
+		
 		panelForm = new JPanel();
 		panelForm.setBackground(SystemColor.window);
-		panelForm.setBounds(398,0,396,600);
+		panelForm.setBounds(login.getWidth()/2,0,login.getWidth()-panelImage.getWidth(),login.getHeight());
 		login.getContentPane().add(panelForm);
 		panelForm.setLayout(null);
 		
@@ -65,13 +93,14 @@ public class Login extends JFrame{
 		labelAccess.setBounds(66,293,265,31);
 		labelAccess.setVisible(false);
 		
+
 		labelCode = new JLabel("Codice accesso: ");
-		labelCode.setBounds(66,190,265,36);
+		labelCode.setBounds(68,190,265,36);
 		labelCode.setFont(new Font("Tahoma",Font.PLAIN,20));
 		panelForm.add(labelCode);
 		
 		textCode = new JTextField();
-		textCode.setBounds(66, 231, 265, 31);
+		textCode.setBounds(68, 231, 265, 31);
 		panelForm.add(textCode);
 		
 		labelWelcome = new JLabel("BENVENUTO", SwingConstants.CENTER);
@@ -79,8 +108,7 @@ public class Login extends JFrame{
 		labelWelcome.setForeground(Color.BLACK);
 		labelWelcome.setFont(new Font("Tahoma",Font.PLAIN,25));
 		panelForm.add(labelWelcome);
-		
-		
+
 		
 	}
 	
