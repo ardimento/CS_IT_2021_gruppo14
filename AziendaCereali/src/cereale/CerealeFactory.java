f135c508ece9648b918bed8f3d12e84022ec83c4
DrBen <DrBen@LAPTOP-3E309KQA>
2021-03-07 16:31:57 +0100

    Creazione del Package eccezioni e delle classi in esso contenute #15
    Classe cerealeFactory: Aggiunti i parametri richiesti per il costruttore
    della classe "Avena" per effettuare il controllo sulla quantità.

--- AziendaCereali/src/cereale/CerealeFactory.java
+++ AziendaCereali/src/cereale/CerealeFactory.java
@@ -1,13 +1,15 @@
 package cereale;
 
+import eccezioni.EccezioniVendita;
+
 public class CerealeFactory {
 
-	public static Cereale creatoreCereale (String cereale) {
+	public static Cereale creatoreCereale (String cereale, double quantitaCereale, double quantitaVenduta) throws EccezioniVendita {
 		Cereale cerealeDaIstanziare = null;
 		switch (cereale) {
 		
 			case "avena" : {
-				cerealeDaIstanziare = new Avena(ProprietaNutritive.PROPRIETA_AVENA);
+				cerealeDaIstanziare = new Avena(ProprietaNutritive.PROPRIETA_AVENA, quantitaCereale, quantitaVenduta);
 			
 			}
 		
