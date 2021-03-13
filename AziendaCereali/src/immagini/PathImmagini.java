13e89617393ea1797898bfb81365510b729187d7
AlemannoGiuseppe <giuseppe.alemanno1100@gmail.com>
2021-03-13 17:17:57 +0100

    Creazione del Package immagini #17
    Aggiornamento :
    
    - inserimento dei nuovi path nella classe 'PathImmagini'.
    - inseriemnto di una cartella contenente le possibili icone da
    utilizzare nel programma.

--- AziendaCereali/src/immagini/PathImmagini.java
+++ AziendaCereali/src/immagini/PathImmagini.java
@@ -15,9 +15,25 @@ public class PathImmagini {
 
 	/** valore alfanumerico che rappresenta il path dell'immagine utilizzata nella schermata @see login */
 	public final static String IMMAGINE_LOGIN = "/immagini/immagine2.jpg";
+	
 	/** valore alfanumerico che rappresenta il path dell'immagine utilizzata come icona nella finestra del programma */
 	public final static String IMMAGINE_ICONA_PROGRAMMA = "/immagini/iconaFinestra.png";
+	
 	/** valore alfanumerico che rappresenta il path dell'immagine utilizzata come icona nei bottoni per tornare indieto */
 	public final static String IMMAGINE_BOTTONE_INDIETRO = "/immagini/freccia1.png";
 	
+	/** valore alfanumerico che rappresenta il path dell'immagine utilizzata come icona dell'utente */
+	public final static String IMMAGINE_ICONA_UTENTE = "/immagini/male1.png";
+	
+	/** valore alfanumerico che rappresenta il path dell'immagine utilizzata come icona del bottone @see btnMakeSell */
+	public final static String IMMAGINE_ICONA_VENDI = "/immagini/wheat-sack-money.png";
+	
+	/** valore alfanumerico che rappresenta il path dell'immagine utilizzata come icona del bottone  @see btnShowSell */
+	public final static String IMMAGINE_ICONA_VISUALIZZA_VENDITE = "/immagini/archive-wheat-sack.png";
+	
+	/** valore alfanumerico che rappresenta il path dell'immagine utilizzata come icona delle vendite giornaliere dell'impiegato */
+	public final static String IMMAGINE_ICONA_VENDITE_GIORNATA = "/immagini/wheat-sack-day.png";
+	
+	/** valore alfanumerico che rappresenta il path dell'immagine utilizzata come icona delle vendite annuali dell'impiegato */
+	public final static String IMMAGINE_ICONA_VENDITE_ANNO= "/immagini/wheat-sack-year.png";
 }
