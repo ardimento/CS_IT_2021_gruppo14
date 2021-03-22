5ac1279c930d9df4fafd123f04d3aac303251cf6
BenSidiAlessio <bensidialessio@gmail.com>
2021-03-22 13:33:52 +0100

    Creazione della classe Factory di cereale #6
    Effettuato l'overload del metodo "creatoreCereale" adattando i
    parametri, presi dal database, per la creazione di cereali all'avvio del
    sistema. E fornita per questo metodo anche la documentazione.

--- AziendaCereali/src/cereale/CerealeFactory.java
+++ AziendaCereali/src/cereale/CerealeFactory.java
@@ -12,8 +12,32 @@ public class CerealeFactory {
 				cerealeDaIstanziare = new Avena(ProprietaNutritive.PROPRIETA_AVENA, quantitaCereale, quantitaVenduta);
 			
 			}
+
+		}
+		return cerealeDaIstanziare;
+	}
+	
+	/**
+	 * Metodo Statico che permette di essere richiamato per la creazione di un Cereale, tramite il parametro stringa "cereale" che fa da selettore
+	 * per il giusto tipo di prodotto da creare.
+	 * 
+	 * @param cereale Stringa che indica il prodotto da dover creare una istanza
+	 * @param quantitaCereale valore double che indica la quantita del cereale venduta in Kilogrammi
+	 * 
+	 * @return una istanza del cereale selezionato
+	 * 
+	 * @pre il parametro @see cereale deve essere una stringa presente tra i prodotti disponibili alla vendita (di cui le loro classi siano definite)
+	 * @post una istanza del cereale richiesto con i dati presi dal database
+	 */
+	public static Cereale creatoreCereale (String cereale, double quantitaCereale) {
+		Cereale cerealeDaIstanziare = null;
+		switch (cereale) {
 		
-		
+			case "avena" : {
+				cerealeDaIstanziare = new Avena(ProprietaNutritive.PROPRIETA_AVENA, quantitaCereale);
+			
+			}
+
 		}
 		return cerealeDaIstanziare;
 	}
